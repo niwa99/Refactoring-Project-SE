da253318621c2af4d00abb09933359696cf5d89a
thjg <info@thjg.de>
2020-05-19 02:58:53 +0200

    Replace Vector by ArrayList

--- src/de/dhbw/src/Customer.java
+++ src/de/dhbw/src/Customer.java
@@ -9,14 +9,14 @@ class Customer {
 	private static final String C_LINEBREAK = "\n";
 	
     private String name;
-    private Vector rentals = new Vector();
+    private ArrayList<Rental> rentals = new ArrayList<>();
     
     public Customer(String newname) {
         name = newname;
     }
     
     public void addRental(Rental arg) {
-        rentals.addElement(arg);
+        rentals.add(arg);
     }
     
     public String getName() {
@@ -27,25 +27,23 @@ class Customer {
     	
         double totalAmount = 0;
         int frequentRenterPoints = 0;
-        Enumeration enum_rentals = rentals.elements();
         String result = "Rental Record for " + this.getName() + C_LINEBREAK;
         result += C_TAB + "Title" + C_TAB + C_TAB + "Days" + C_TAB + "Amount" + C_LINEBREAK;
-
-        while (enum_rentals.hasMoreElements()) {
-            double thisAmount = 0;
-            Rental each = (Rental) enum_rentals.nextElement();
-            //determine amounts for each line
-            thisAmount = amountFor(each);
-            // add frequent renter points
-            frequentRenterPoints ++;
-            // add bonus for a two day new release rental
-            if ((each.getMovie().getPriceCode() == PriceCode.NEW_RELEASE) && each.getDaysRented() > 1) {
-                frequentRenterPoints ++;
-            }
-            //show figures for this rental
-            result += C_TAB + each.getMovie().getTitle()+ C_TAB + C_TAB + each.getDaysRented() + C_TAB + String.valueOf(thisAmount) + C_LINEBREAK;
-            totalAmount += thisAmount;
-        }
+        
+        for (Rental rental : rentals) {
+        	double thisAmount = 0;
+        	thisAmount = amountFor(rental);
+        	frequentRenterPoints ++;
+        	
+        	// add bonus for a two day new release rental
+        	if ((rental.getMovie().getPriceCode() == PriceCode.NEW_RELEASE) && rental.getDaysRented() > 1) {
+        		frequentRenterPoints ++;
+        	}
+        	
+        	//show figures for this rental
+        	result += C_TAB + rental.getMovie().getTitle()+ C_TAB + C_TAB + rental.getDaysRented() + C_TAB + String.valueOf(thisAmount) + C_LINEBREAK;
+        	totalAmount += thisAmount;
+        };
         //add footer lines
         result += "Amount owed is " + String.valueOf(totalAmount) + C_LINEBREAK;
         result += "You earned " + String.valueOf(frequentRenterPoints) + " frequent renter points";
